639d621692c314c8071550204bf950e0e415503f
Atul Thakur <informatulthakur@gmail.com>
2026-08-19 13:03:49 +0530

    Add: reorganize-string - submission-1

---
+++ Data Structures & Algorithms/reorganize-string/submission-1.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int n = s.size();
+        string ans = "";
+
+        priority_queue<pair<int,char>> pq;
+        vector<int> freq(26, 0);
+
+        for(char ch : s) {
+            freq[ch - 'a']++;
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if(freq[i] > 0) {
+                pq.push({freq[i], 'a' + i});
+            }
+        }
+        
+        char prev = '#';
+
+        while(!pq.empty()) {
+
+            auto temp = pq.top();
+            pq.pop();
+
+            int count = temp.first;
+            char ch = temp.second;
+
+            if(prev != ch) {
+                ans += ch;
+                prev = ch;
+                count--;
+
+                if(count > 0) {
+                    pq.push({count, ch});
+                }
+            }
+            else {
+
+                if(pq.empty()) {
+                    return "";
+                }
+
+                auto temp2 = pq.top();
+                pq.pop();
+
+                int count2 = temp2.first;
+                char ch2 = temp2.second;
+
+                ans += ch2;
+                prev = ch2;
+                count2--;
+
+                if(count2 > 0) {
+                    pq.push({count2, ch2});
+                }
+
+                pq.push({count, ch});
+            }
+        }
+
+        return ans;
+    }
+};
